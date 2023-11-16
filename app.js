69eb20a988b073fcfa0b8b70e969c34fecf42969
AinzsOalGoan <deep951india@gmail.com>
2023-11-16 19:58:10 +0530

    Added index.ejs page

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//make the full setup required for the views to show after then dm me i will work.
 
-//root rout
+//root route
 app.get('/',(req,res)=>{
   res.send("todo app");
 });
 
-
+//index route
+app.get('/index',(req,res)=>{
+  res.render('index.ejs');
+});
 
 
 app.listen(port,()=>{
