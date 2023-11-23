ecb8619a173495e3f965f50033ca6fce1e6fe3c9
ANIRBAN SANTRA <129375292+anirbansantra748@users.noreply.github.com>
2023-11-24 00:37:53 +0530

    initilise the detabace add form to get data

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # TodoList
 making a Todo list web app 
 #23/11/23
-Created index route and index page is still in phase 1a.
+
+#soumadeep part 
+
+
+#op's part
+1. initilise the detabace add form to get data 
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,12 +15,38 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.engine("ejs", ejsMate);
 
+//NOTE - Initilise Mongoose here
+const mongo_url = 'mongodb://127.0.0.1:27017/TODOAPP';
+
+async function main() {
+  await mongoose.connect(mongo_url);
+}
+
+main()
+  .then(() => {
+    console.log('Mongoose started');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //index route
 app.get("/home",(req,res)=>{
   res.render("pages/index.ejs")}
 );
 
+//NOTE - add task get rout to serve form
+app.get("/add",(req,res)=>{
+  res.render("pages/addForm.ejs");
+});
+//NOTE - add task post route for save in databace
+app.post("/add",(req,res)=>{
+  let task = req.body.task;
+  let duration = req.body.duration;
+  let date = req.body.date;
+  console.log(task);
+  res.send(task +" ki maki chut. Land kare aise task");
+})
 //root route
 app.get('/',(req,res)=>{
   res.send("todo app");
