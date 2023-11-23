fc3f20168b318a147105e5ae43ee0c9b1ad278ad
AinzsOalGoan <deep951india@gmail.com>
2023-11-23 16:29:21 +0530

    Added index page and updated app.js index page is in pase 1a

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-//make the full setup required for the views to show after then dm me i will work.
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.engine("ejs", ejsMate);
+
+
+//index route
+app.get("/home",(req,res)=>{
+  res.render("pages/index.ejs")}
+);
 
 //root route
 app.get('/',(req,res)=>{
   res.send("todo app");
 });
-
-app.get("/home",(req,res)=>{
-  res.render("pages/index.ejs")});
-
 
 app.listen(port,()=>{
   console.log("app is started");
